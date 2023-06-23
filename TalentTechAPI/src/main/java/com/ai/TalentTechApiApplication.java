234a15b0b069964b42716cc85e67b4c188cec83b
prathmesh49 <prathmesh_kurane@yahoo.com>
2023-06-23 12:45:56 +0530

    api initial

---
+++ TalentTechAPI/src/main/java/com/ai/TalentTechApiApplication.java
@@ -0,0 +1,13 @@
+package com.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalentTechApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalentTechApiApplication.class, args);
+	}
+
+}
