220dad8290453c1a6d2fd8e0999af637f3afb13e
SanketPotdar121695 <sanket.potdar563@gmail.com>
2023-06-24 12:23:29 +0530

    added authentication in the backend

--- backend/index.js
+++ backend/index.js
@@ -1,31 +1,23 @@
+const express = require('express');
+const { connection } = require('./config/db');
+const { userRouter } = require('./routes/user.routes');
+require('dotenv').config();
 
+const app = express();
 
-const express=require("express")
-const { connection } = require("./db")
-require("dotenv").config()
+app.use(express.json());
+app.use('/users', userRouter);
 
+app.get('/', (req, res) => {
+  res.send('home route');
+});
 
-const app=express()
-
-app.use(express.json())
-
-
-
-
-
-app.get("/",(req,res)=>{
-    res.send("home route")
-})
-
-
-
-
-app.listen(process.env.port,async()=>{
-    try {
-        await connection
-        console.log("db is connected")
-    } catch (error) {
-        console.log(error)
-    }
-    console.log("server is running")
-})
+app.listen(process.env.port, async () => {
+  try {
+    await connection;
+    console.log('db is connected');
+  } catch (error) {
+    console.log(error);
+  }
+  console.log('server is running');
+});
